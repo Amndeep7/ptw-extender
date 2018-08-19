37fa23b83ab1a7974440c24e8f22f2a941cc5b4a
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-08-19 18:10:06 -0400

    Bug fix to the save code so I wouldn't overwrite all the options with just one option

--- options/options.js
+++ options/options.js
@@ -3,11 +3,17 @@
 const saveCheckboxOption = async (option) => {
 	console.log(option);
 	try {
+		// objects get fully replaced, so having the optionSource be the key made it so every update
+		// would wipe the contents of the optionSource - resolution is to get the contents of the optionSource
+		// every time a change is desired, change the the contents accordingly, and then push the contents back up.
+		// wasteful, but it helps maintain organization and it's not like there's lots of options nor will a user be
+		// constantly changing them so i'm fine with it
+		const options = await browser.storage.sync.get(option.target.dataset.optionSource);
+		options[option.target.dataset.optionSource][option.target.name] = option.target.checked;
 		await browser.storage.sync.set({
-			[option.target.dataset.optionSource]: {
-				[option.target.name]: option.target.checked,
-			},
+			[option.target.dataset.optionSource]: options[option.target.dataset.optionSource]
 		});
+
 		document.querySelector("#results").innerHTML = `${option.target.name}`
 			+ ` was successfully saved to ${option.target.checked}`;
 	} catch (e) {
