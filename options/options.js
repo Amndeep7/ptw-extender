6bfc839a9bdec1d7060743d93f03bb71fa7db8c3
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-08-22 20:31:22 -0400

    Reworked saving+restoring and added disabling

--- options/options.js
+++ options/options.js
@@ -1,40 +1,32 @@
 /* eslint no-console: "off" */
 
-const saveCheckboxOption = async (option) => {
-	console.log(option);
-	try {
-		// objects get fully replaced, so having the optionSource be the key made it so every update
-		// would wipe the contents of the optionSource - resolution is to get the contents of the optionSource
-		// every time a change is desired, change the the contents accordingly, and then push the contents back up.
-		// wasteful, but it helps maintain organization and it's not like there's lots of options nor will a user be
-		// constantly changing them so i'm fine with it
-		const options = await browser.storage.sync.get(option.target.dataset.optionSource);
-		options[option.target.dataset.optionSource][option.target.name] = option.target.checked;
-		await browser.storage.sync.set({
-			[option.target.dataset.optionSource]: options[option.target.dataset.optionSource]
-		});
-
-		document.querySelector("#results").innerHTML = `${option.target.name}`
-			+ ` was successfully saved to ${option.target.checked}`;
-	} catch (e) {
-		console.log("error while saving", e);
-		document.querySelector("#results").innerHTML = `${option.target.name} was not successfully saved`;
-	}
+// names have to be unique, but I wanna allow the possibility of the same name for different option sources
+// so instead of being able to use an html data attribute or something, I gotta stick the source in the name
+const optionsDefaults = {
+	"checkbox": {
+		"extension_displayNotifications": true,
+		"extension_prettifyCommentsBox": true,
+		"mal_mal": true,
+		"mal_autosubmit": false,
+	},
+	"radio": {
+		"mal_behaviorPostAutosubmit": "titlePage",
+	},
 };
 
-const restoreOptions = async (type, defaults, restoreOption) => {
+const restoreCheckboxOption = (option, checked) => {
+	document.querySelector(`[name="${option}"]`).checked = checked;
+};
+
+const restoreRadioOption = (option, value) => {
+	document.querySelector("form").elements[option].value = value;
+};
+
+const restoreOptions = (options, type, restoreOption) => {
 	console.log(`restoring ${type} options`);
-
 	try {
-		const options = await browser.storage.sync.get(defaults);
-		// this is to make sure the defaults are assigned in the first place,
-		// but not override any settings that did get changed
-		await browser.storage.sync.set(options);
-
-		Object.entries(options).forEach((optionSource) => {
-			Object.entries(optionSource[1]).forEach((option) => {
-				restoreOption(optionSource[0], option[0], option[1]);
-			});
+		Object.entries(options[type]).forEach((option) => {
+			restoreOption(option[0], option[1]);
 		});
 
 		document.querySelector("#results").innerHTML = `Successfully restored ${type} options from memory`;
@@ -44,22 +36,105 @@ const restoreOptions = async (type, defaults, restoreOption) => {
 	}
 };
 
-const checkboxOptionsDefaults = {
-	"extension": {
-		"displaynotifications": true,
-		"prettifycommentsbox": true,
-	},
-	"mal": {
-		"autosubmit": false,
-	},
+const saveOption = async (option, type) => {
+	console.log(option);
+	try {
+		// objects get fully replaced, so having the optionSource be the key made it so every update
+		// would wipe the contents of the optionSource - resolution is to get the contents of the optionSource
+		// every time a change is desired, change the the contents accordingly, and then push the contents back up.
+		// wasteful, but it helps maintain organization and it's not like there's lots of options nor will a user be
+		// constantly changing them so i'm fine with it
+		const options = await browser.storage.sync.get(type.type);
+		options[type.type][option.target.name] = option.target[type.property];
+		await browser.storage.sync.set({
+			[type.type]: options[type.type],
+		});
+
+		document.querySelector("#results").innerHTML = `${option.target.name}`
+			+ ` was successfully saved to ${option.target[type.property]}`;
+	} catch (e) {
+		console.log("error while saving", e);
+		document.querySelector("#results").innerHTML = `${option.target.name} was not successfully saved`;
+	}
 };
 
-const restoreCheckboxOption = (optionSource, option, checked) => {
-	document.querySelector(`[data-option-source="${optionSource}"][name="${option}"]`).checked = checked;
+const setupSavingCheckboxOptions = () => {
+	document.querySelectorAll("input[type=checkbox]")
+		.forEach((option) => option.addEventListener("change", (o) => {
+			saveOption(o, { "type": "checkbox", "property": "checked" });
+		}));
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-	restoreOptions("checkbox", checkboxOptionsDefaults, restoreCheckboxOption);
+const setupSavingRadioOptions = () => {
+	document.querySelectorAll("input[type=radio]")
+		.forEach((option) => option.addEventListener("change", (o) => {
+			saveOption(o, { "type": "radio", "property": "value" });
+		}));
+};
+
+const setupDisablingDependentOptions = () => {
+	// dependent class and associated attributes should only be applied to wrapping divs
+	document.querySelectorAll(".dependent")
+		.forEach((dependency) => {
+			console.log("dependency", dependency);
+			// might need to come back to add more tags other than input
+			dependency.querySelectorAll("input").forEach((input) => {
+				console.log("input", input);
+				const value = ((v) => {
+					// https://stackoverflow.com/a/23752239/645647 - example on how to use empty + custom data attributes
+					// undefined means attribute not there, "" means attribute there using empty attribute syntax or
+					// explicitly set to that, anything else will be the value there - so to have true, the restriction
+					// will be that value will have to have nonempty strings if you want anything in there besides a bool
+					switch (v) {
+					case undefined:
+						return false;
+					case "":
+						return true;
+					default:
+						return v;
+					}
+				})(dependency.dataset.dependentPropertyValue);
+
+				if (input.dependencies === undefined) {
+					// eslint-disable-next-line no-param-reassign
+					input.dependencies = {};
+				}
+				const option = document.querySelector(`input[name="${dependency.dataset.dependentOn}"]`);
+				console.log("option", option);
+				const update = (o) => {
+					console.log(input, o, input.dependencies);
+					// eslint-disable-next-line no-param-reassign
+					input.dependencies[dependency.dataset.dependentOn] = o[dependency.dataset.dependentProperty]
+						=== value;
+					// eslint-disable-next-line no-param-reassign
+					input.disabled = Object.values(input.dependencies).reduce((acc, cur) => acc || cur);
+				};
+				update(option);
+				option.addEventListener("change", (o) => {
+					console.log("o", o);
+					update(o.target);
+				});
+			});
+		});
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+	// make sure the defaults are assigned, but don't override any changed settings
+	let options = null;
+	try {
+		options = await browser.storage.sync.get(optionsDefaults);
+		await browser.storage.sync.set(options);
+	} catch (e) {
+		console.log("error while restoring while in listener", e);
+		document.querySelector("#results").innerHTML = "Didn't successfully assign default/unchanged options";
+		throw e;
+	}
+
+	restoreOptions(options, "checkbox", restoreCheckboxOption);
+	restoreOptions(options, "radio", restoreRadioOption);
+
+	setupSavingCheckboxOptions();
+	setupSavingRadioOptions();
+
+	setupDisablingDependentOptions();
 });
-document.querySelectorAll("input[type=checkbox]")
-	.forEach((option) => option.addEventListener("change", saveCheckboxOption));
