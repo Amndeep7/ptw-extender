729547c1b031bb6f5c1547678084660a187e54bc
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-08-24 15:31:29 -0400

    Fixed bug #21 - Don't change status if it's not watching or if episodes/volumes/chapters are listed as watched

--- mal/sourceadder.js
+++ mal/sourceadder.js
@@ -9,6 +9,7 @@ browser.runtime.onMessage.addListener((message, sender) => {
 			const advanced = document.querySelector(".advanced");
 			advanced.style = "";
 			console.log("displaying advanced section");
+
 			const comments = document.querySelector(`#add_${message.type}_comments`);
 			console.log(`${comments.value.length}`);
 			console.log(`${comments.value.length === 0}`);
@@ -20,13 +21,36 @@ browser.runtime.onMessage.addListener((message, sender) => {
 				comments.value += `${"\n"}${message.taburl}`;
 			}
 			console.log("text added");
+
 			const status = document.querySelector(`#add_${message.type}_status`);
 			console.log(status);
 			console.log(status.id);
 			console.log(status.value);
 			console.log(status.options);
-			status.value = 6;
-			console.log("status changed");
+			// only change status if it's watching/reading cause watching/reading is the default status
+			if (status.value === "1") {
+				console.log("status is watching");
+				// only change status if the count is at 0 which presumably means that user hasn't started it
+				const haventstarted = ["", 0, "0"];
+				switch (message.type) {
+				case "anime":
+					if (haventstarted.includes(document.querySelector("#add_anime_num_watched_episodes").value)) {
+						status.value = 6;
+						console.log("status changed");
+					}
+					break;
+				case "manga":
+					if (haventstarted.includes(document.querySelector("#add_manga_num_read_volumes").value)
+						&& haventstarted.includes(document.querySelector("#add_manga_num_read_chapters").value)) {
+						status.value = 6;
+						console.log("status changed");
+					}
+					break;
+				default:
+					console.log("message.type is unknown", message.type);
+					return Promise.resolve(false);
+				}
+			}
 			return Promise.resolve(true);
 		}
 	}
