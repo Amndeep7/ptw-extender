98040fdf9b765cf3edd2a4f4ce7cfba12392976d
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-08-24 21:18:52 -0400

    Started work with adding in options - Reworked mine_url func to be more generalized - Added more notifications: #22, #16 - Added options global object and updater - Added options to whether or not a notification is shown: #16

--- menu.js
+++ menu.js
@@ -2,8 +2,7 @@
 
 const menuId = "ptw";
 console.log("removing context menus");
-const remove = browser.contextMenus.removeAll();
-remove.then(() => {
+browser.contextMenus.removeAll().then(() => {
 	console.log("creating context menu");
 	browser.contextMenus.create({
 		"id": menuId,
@@ -12,27 +11,52 @@ remove.then(() => {
 	});
 }).catch((e) => {
 	console.log("error while deleting context menus", e);
+	throw e;
 });
 
-const Sites = Object.freeze({ "mal": Symbol("mal") });
+let options = {};
+browser.storage.sync.get().then((o) => {
+	options = o;
+}).catch((e) => {
+	console.log("error while getting options", e);
+	throw e;
+});
+browser.storage.onChanged.addListener((changes, _areaName) => {
+	console.log("option was changed");
+	Object.entries(changes).forEach((change) => {
+		options[change[0]] = change[1].newValue;
+	});
+});
 
-const validateAndMineURL = (url) => {
-	// eslint-disable-next-line no-undef
-	const matchMAL = matchOnMAL(url);
-	if (matchMAL) {
-		matchMAL.source = Sites.mal;
-		return matchMAL;
+const Sites = Object.freeze({ "mal": Symbol("MyAnimeList") });
+
+const validateAndMineURL = (url, notIgnoring) => {
+	const matchFuncs = {
+		"mal": matchOnMAL, // eslint-disable-line no-undef
+	};
+
+	// eslint-disable-next-line no-restricted-syntax
+	for (const site of Object.keys(matchFuncs)) {
+		const match = matchFuncs[site](url);
+		if (match) {
+			match.source = Sites[site];
+			match.notIgnoring = notIgnoring[site];
+			return match;
+		}
 	}
+
 	return false;
 };
 
 const createNotification = (notification) => {
-	browser.notifications.create({
-		"type": "basic",
-		"iconUrl": browser.extension.getURL("./icons/icon_48.png"),
-		"title": notification.title,
-		"message": notification.message,
-	});
+	if (options.checkbox.extension_displayNotifications) {
+		browser.notifications.create({
+			"type": "basic",
+			"iconUrl": browser.extension.getURL("./icons/icon_48.png"),
+			"title": notification.title,
+			"message": notification.message,
+		});
+	}
 };
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
@@ -40,12 +64,25 @@ browser.contextMenus.onClicked.addListener(async (info, tab) => {
 	if (info.menuItemId === menuId) {
 		console.log(`Link URL: ${info.linkUrl}`);
 		console.log(`Tab URL: ${tab.url}`);
-		const urlData = validateAndMineURL(info.linkUrl);
-		if (urlData.source === Sites.mal) {
+		const notIgnoring = {
+			"mal": options.checkbox.mal_mal,
+		};
+		const urlData = validateAndMineURL(info.linkUrl, notIgnoring);
+		if (urlData && !urlData.notIgnoring) {
+			console.log("Match ignored");
+			createNotification({
+				"title": "Ignoring list site",
+				"message": `PTW Extender is currently set to ignore ${urlData.source.toString().slice(7, -1)}`,
+			}, options.checkbox.extension_displayNotifications);
+		} else if (urlData && urlData.source === Sites.mal) {
 			// eslint-disable-next-line no-undef
 			createNotification(await handleMAL(tab, urlData));
 		} else {
 			console.log("Match fail");
+			createNotification({
+				"title": "Unknown list site",
+				"message": "PTW Extender isn't compatible with that site",
+			});
 		}
 		console.log("world");
 	}
