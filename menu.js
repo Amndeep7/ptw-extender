b70c25ce4af07c7dd50c7bf3a5ee76a437524be6
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-10-24 18:17:02 -0400

    Changed from being if(x){y} to be if(!x){return}y

--- menu.js
+++ menu.js
@@ -109,55 +109,56 @@ browser.contextMenus.removeAll()
 
 			browser.contextMenus.onClicked.addListener(async (info, tab) => {
 				console.log("hello");
-				if (info.menuItemId === menuId) {
-					console.log(`Link URL: ${info.linkUrl}`);
-					console.log(`Tab URL: ${tab.url}`);
-					const notIgnoring = {
-						"mal": options.checkbox.mal_mal,
-						"anilist": options.checkbox.anilist_anilist,
-						"kitsu": options.checkbox.kitsu_kitsu,
-					};
-					const urlData = validateAndMineURL(info.linkUrl, notIgnoring);
-					if (urlData && !urlData.notIgnoring) {
-						console.log("Match ignored");
-						createNotification({
-							"title": "Ignoring list site",
-							"message": `PTW Extender is currently set to ignore ${urlData.source
-								.toString().slice(7, -1)}`,
-						});
-					} else if (urlData && urlData.source === Sites.mal) {
-						// eslint-disable-next-line no-undef
-						createNotification(await handleMAL(tab, urlData, {
-							"prettifyCommentsBox": options.checkbox.extension_prettifyCommentsBox,
-							"autosubmit": options.checkbox.mal_autosubmit,
-							"behaviorPostAutosubmit": options.radio.mal_behaviorPostAutosubmit,
-							"priority": options.radio.mal_priority,
-							"tags": options.textarea.mal_tags,
-						}));
-					} else if (urlData && urlData.source === Sites.anilist) {
-						// eslint-disable-next-line no-undef
-						createNotification(await handleAniList(tab, urlData, {
-							"accessToken": optionsLocal.authentication.anilist.accessToken,
-							"private": options.checkbox.anilist_private,
-							"hiddenFromStatusLists": options.checkbox.anilist_hiddenFromStatusLists,
-							"customListsAnime": options.multipleCheckbox.anilist_customListsAnime,
-							"customListsManga": options.multipleCheckbox.anilist_customListsManga,
-						}));
-					} else if (urlData && urlData.source === Sites.kitsu) {
-						// eslint-disable-next-line no-undef
-						createNotification(await handleKitsu(tab, urlData, {
-							"accessToken": optionsLocal.authentication.kitsu.accessToken,
-							"private": options.checkbox.kitsu_private,
-						}));
-					} else {
-						console.log("Match fail");
-						createNotification({
-							"title": "Unknown list site",
-							"message": "PTW Extender isn't compatible with that site",
-						});
-					}
-					console.log("world");
+				if (!([menuId].includes(info.menuItemId))) {
+					return;
 				}
+				console.log(`Link URL: ${info.linkUrl}`);
+				console.log(`Tab URL: ${tab.url}`);
+				const notIgnoring = {
+					"mal": options.checkbox.mal_mal,
+					"anilist": options.checkbox.anilist_anilist,
+					"kitsu": options.checkbox.kitsu_kitsu,
+				};
+				const urlData = validateAndMineURL(info.linkUrl, notIgnoring);
+				if (urlData && !urlData.notIgnoring) {
+					console.log("Match ignored");
+					createNotification({
+						"title": "Ignoring list site",
+						"message": `PTW Extender is currently set to ignore ${urlData.source
+							.toString().slice(7, -1)}`,
+					});
+				} else if (urlData && urlData.source === Sites.mal) {
+					// eslint-disable-next-line no-undef
+					createNotification(await handleMAL(tab, urlData, {
+						"prettifyCommentsBox": options.checkbox.extension_prettifyCommentsBox,
+						"autosubmit": options.checkbox.mal_autosubmit,
+						"behaviorPostAutosubmit": options.radio.mal_behaviorPostAutosubmit,
+						"priority": options.radio.mal_priority,
+						"tags": options.textarea.mal_tags,
+					}));
+				} else if (urlData && urlData.source === Sites.anilist) {
+					// eslint-disable-next-line no-undef
+					createNotification(await handleAniList(tab, urlData, {
+						"accessToken": optionsLocal.authentication.anilist.accessToken,
+						"private": options.checkbox.anilist_private,
+						"hiddenFromStatusLists": options.checkbox.anilist_hiddenFromStatusLists,
+						"customListsAnime": options.multipleCheckbox.anilist_customListsAnime,
+						"customListsManga": options.multipleCheckbox.anilist_customListsManga,
+					}));
+				} else if (urlData && urlData.source === Sites.kitsu) {
+					// eslint-disable-next-line no-undef
+					createNotification(await handleKitsu(tab, urlData, {
+						"accessToken": optionsLocal.authentication.kitsu.accessToken,
+						"private": options.checkbox.kitsu_private,
+					}));
+				} else {
+					console.log("Match fail");
+					createNotification({
+						"title": "Unknown list site",
+						"message": "PTW Extender isn't compatible with that site",
+					});
+				}
+				console.log("world");
 			});
 		}, 50);
 	});
